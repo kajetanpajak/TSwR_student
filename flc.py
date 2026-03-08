04e7c1aea411117d16dbb0657cf666a98619a0ce
kajetanpajak <kajpaj707@wp.pl>
2026-03-08 20:15:03 +0100

    implemented flc

--- flc.py
+++ flc.py
@@ -10,13 +10,13 @@ from utils.simulation import simulate
 
 Tp = 0.01
 start = 0
-end = 3
+end = 5
 
 """
 Switch to FeedbackLinearizationController as soon as you implement it
 """
-#controller = FeedbackLinearizationController(Tp)
-controller = DummyController(Tp)
+controller = FeedbackLinearizationController(Tp)
+# controller = DummyController(Tp)
 
 """
 Here you have some trajectory generators. You can use them to check your implementations.
@@ -35,12 +35,15 @@ You can add here some plots of the state 'Q' (consists of q and q_dot), controls
 with respect to time 'T' to analyze what is going on in the system
 """
 plt.subplot(221)
-plt.plot(T, Q[:, 0], 'r')
-plt.plot(T, Q_d[:, 0], 'b')
+plt.plot(T, Q[:, 0], 'r', label=r'$q_1(t)$')
+plt.plot(T, Q_d[:, 0], 'b', label=r'$q_{1d}(t)$')
+plt.legend()
 plt.subplot(222)
-plt.plot(T, Q[:, 1], 'r')
-plt.plot(T, Q_d[:, 1], 'b')
+plt.plot(T, Q[:, 1], 'r', label=r'$q_2(t)$')
+plt.plot(T, Q_d[:, 1], 'b', label=r'$q_{2d}(t)$')
+plt.legend()
 plt.subplot(223)
-plt.plot(T, u[:, 0], 'r')
-plt.plot(T, u[:, 1], 'b')
+plt.plot(T, u[:, 0], 'r', label=r'$\tau_1(t)$')
+plt.plot(T, u[:, 1], 'b', label=r'$\tau_2(t)$')
+plt.legend()
 plt.show()
--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -12,9 +12,27 @@ class ManiuplatorModel:
         self.m2 = 1.
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
-        self.m3 = 0.0
-        self.r3 = 0.01
+        self.m3 = 1.
+        self.r3 = 0.05
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
+
+        print(self.I_3)
+
+        self.alpha = (
+            self.m1 * ((self.l1 * 0.5) ** 2) + self.I_1
+            + self.m2 * (self.l1 ** 2 + (self.l2 * 0.5) ** 2)
+            + self.I_2 + self.m3 * (self.l2 ** 2) + self.I_3
+        )
+
+        self.beta = (
+            self.m2 * ((self.l2 * 0.5) ** 2) + self.I_2
+            + self.m3 * (self.l2 ** 2) + self.I_3
+        )
+
+        self.gamma = (
+            self.m2 * self.l1 * (self.l2 * 0.5)
+            + self.m3 * self.l1 * self.l2
+        )
 
     def M(self, x):
         """
@@ -22,7 +40,20 @@ class ManiuplatorModel:
         (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        m11 = self.alpha + 2 * self.gamma * np.cos(q2)
+        m21 = self.beta +  self.gamma * np.cos(q2)
+        m12 = m21
+        m22 = self.beta
+
+        M = np.array(
+                    [[m11, m12],
+                     [m21, m22]]
+        )
+
+        return M
+
+        # return NotImplementedError()
 
     def C(self, x):
         """
@@ -30,4 +61,17 @@ class ManiuplatorModel:
         in the exercise (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        c11 = -2 * self.gamma * q2_dot * np.sin(q2)
+        c21 = self.gamma * q1_dot * np.sin(q2)
+        c12 = -self.gamma * q2_dot * np.sin(q2)
+        c22 = 0
+
+        C = np.array(
+            [[c11, c12],
+             [c21, c22]]
+        )
+
+        return C
+
+        # return NotImplementedError()
